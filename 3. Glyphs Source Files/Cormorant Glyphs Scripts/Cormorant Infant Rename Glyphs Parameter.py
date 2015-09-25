d13314e1a41429f68969e6252fc5e8042a3b0ff3
Christian Thalmann <christian.d.thalmann@gmail.com>
2015-09-25 21:52:01 +0200

    Added a proper italic to Infant; Infant now uses lining figures

---
+++ 3. Glyphs Source Files/Cormorant Glyphs Scripts/Cormorant Infant Rename Glyphs Parameter.py
@@ -0,0 +1,29 @@
+#MenuTitle: Cormorant Infant Rename Glyphs Custom Parameter
+# -*- coding: utf-8 -*-
+"""Injects and/or updates the Rename Glyphs custom parameters for all upright italics in the .glyphs file."""
+
+import GlyphsApp
+
+# Infant
+Font = Glyphs.font
+
+suffix = ".ss03"
+allSuffixedGlyphNames = [ g.name for g in Font.glyphs if g.name.endswith(suffix) ]
+renameGlyphs1 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in allSuffixedGlyphNames ]
+renameGlyphsParameterKey = "Rename Glyphs"
+
+suffix = ".lf"
+allSuffixedGlyphNames = [ g.name for g in Font.glyphs if g.name.endswith(suffix) ]
+renameGlyphs2 = [ "%s=%s" % ( x, x.replace(suffix,"") ) for x in allSuffixedGlyphNames ]
+
+
+for thisInstance in Font.instances:
+	parameterFamilyName = thisInstance.customValueForKey_("familyName")
+	if parameterFamilyName:
+		familyName = parameterFamilyName
+	else:
+		familyName = Font.familyName
+	if familyName.startswith("Cormorant Infant"):
+		thisInstance.removeObjectFromCustomParametersForKey_( renameGlyphsParameterKey )
+		thisInstance.setCustomParameter_forKey_( renameGlyphs1+renameGlyphs2, renameGlyphsParameterKey )
+
